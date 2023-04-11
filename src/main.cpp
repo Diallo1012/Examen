dc3459174b05b3d32514509def5a53a56ec45cfe
Diallo <ibdiallo1012@gmail.com>
2023-04-10 21:36:53 -0300

    mon deuxieme commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
+//inclusions de bibliothèques
 #include <Arduino.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <WebServer.h>
+#include <SPI.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+/*#define BME_SCK  (13)
+#define BME_MISO (12)
+#define BME_MOSI (11)
+#define BME_CS   (10)*/
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // la communication I2C
+
+const char *ssid = "Diallo";  // Le nom de notre WIFI
+const char *password = "12345ib."; // Le mot de passe du WIFI
+
+//configuration de notre serveur Web sur le port 80.
+AsyncWebServer server(80);
+
+void handle_NotFound();
+
+const char Home_html[] PROGMEM = R"rawliteral(
