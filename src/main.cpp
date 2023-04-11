2e9d05af293b52b2dfb6f63838370296bdeab209
Diallo <ibdiallo1012@gmail.com>
2023-04-10 21:37:41 -0300

    mon troisieme commit

--- src/main.cpp
+++ src/main.cpp
@@ -28,3 +28,105 @@ AsyncWebServer server(80);
 void handle_NotFound();
 
 const char Home_html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE html> 
+<html lang="en">
+
+<head>
+    <meta content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <style>
+      .ib1{
+    margin-top:60px;
+    background: red;
+    height: 100%;
+    display: block;  
+}
+.container {
+    background-color:whitesmoke;
+    width: 400px;
+}
+.elem1 {
+    background-color: blueviolet;
+}
+.bordure {
+    border: solid white 2px;
+    border-radius: 8px;
+    margin: 20px;
+    background-color: white;
+    font-size: 0.8em;
+}
+.element {
+    font-size: 20px;
+    background-color: whitesmoke;
+    padding: 20px;
+    margin: 20px;
+    box-shadow: 6px 6px 25px rgba(0, 0, 0, 0.5);
+    text-align: center;
+}
+.ib2{
+    color: rgb(6, 133, 88);
+}
+.ib3{
+    color: rgb(45, 98, 173);
+}
+.ib4{
+    color: rgb(72, 169, 103);
+}
+.ib5{
+    color: rgb(149, 26, 26);
+}
+.ib6{
+    padding: 5px;
+    border: white;
+    top: 10%;
+    left: 35vw;
+}
+    </style>
+</head>
+
+<body class="text-center ib1">
+
+    <div class="container">
+        <div class="row elem1">
+            <h2>BME280 WEB SERVER</h2>
+        </div>
+        <div class="ib6">
+            <div class="row bordure element ib2">
+                <div class="row">
+                    <h5><span><i class="bi bi-thermometer-half temp-icon"></i> TEMPERATURE </h5></span>
+                </div>
+                <div><span id="tempVal"></span><sup> &deg;C</sup></div>
+            </div>
+            <div class="row bordure element ib3">
+                <div class="row">
+                    <h5><i class="bi bi-cloud-drizzle humid-icon"></i> HUMIDITY </h5>
+                </div>
+                <div><span id="humidVal"></span><sup> %</sup></div>
+            </div>
+            <div class="row bordure element ib4">
+                <div class="row">
+                    <h5><i class="bi bi-clock-history pressure-icon"></i> PRESSURE </h5>
+                </div>
+                <div><span id="pressVal"></span><sup> hPa</sup></div>
+            </div>
+            <div class="row bordure element ib5">
+                <div class="row">
+                    <h5><i class="bi bi-triangle altitude-icon"></i> ALTITUDE </h5>
+                </div>
+                <div><span id="altVal"></span><sup> M</sup></div>
+            </div>
+        </div>
+    </div>
+
+    <script src="/js/stock.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+</html>)rawliteral";
